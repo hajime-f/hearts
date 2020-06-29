a4b4211bcccb53156df5bc6c5d939740df12c0e4
Hajime Fujita <hajime.fujita@gmail.com>
2020-06-29 21:26:10 +0900

    Commit for coding on multiple environments

---
+++ settings.py
@@ -0,0 +1,14 @@
+# Number of suits: Spade, Heart, Diamond and Club
+NUM_SUITS = 4
+
+# Number of cards in each suit: 2-10, J, Q, K and A
+NUM_KC = 13
+
+# Number of all cards
+NUM_CARDS = NUM_SUITS * NUM_KC
+
+# Total number of games
+NUM_GAMES = 100000
+
+# Number of players: Hearts is played by four players.
+NUM_PR = 4
