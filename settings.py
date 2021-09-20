4f0627005168229a5c2e49a654c4dab4a518e512
Hajime Fujita <hajime.fujita@gmail.com>
2021-09-20 12:25:27 +0900

    作り直し

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
-DEBUG_MODE = 1
+# Total number of games
+NUM_GAMES = 100
 
 # Number of suits: Spade, Heart, Diamond and Club
 NUM_SUITS = 4
@@ -14,16 +15,13 @@ NUM_KC = 13
 # Number of all cards
 NUM_CARDS = NUM_SUITS * NUM_KC
 
-# Total number of games
-NUM_GAMES = 100
-
 # Number of players: Hearts is played by four players.
 NUM_PR = 4
 
 C_2 = 0
-S_Q = SPADE * NUM_KC + 10
-S_K = SPADE * NUM_KC + 11
-S_A = SPADE * NUM_KC + 12
+S_Q = SPADE * NUM_KC + (NUM_KC - 3)
+S_K = SPADE * NUM_KC + (NUM_KC - 2)
+S_A = SPADE * NUM_KC + (NUM_KC - 1)
 H_2 = HEART * NUM_KC
 
 # Penalty points
@@ -37,4 +35,4 @@ FILE_NAME = 'result.csv'
 CARD_NAME = ['C-2', 'C-3', 'C-4', 'C-5', 'C-6', 'C-7', 'C-8', 'C-9', 'C-10', 'C-J', 'C-Q', 'C-K', 'C-A',
              'D-2', 'D-3', 'D-4', 'D-5', 'D-6', 'D-7', 'D-8', 'D-9', 'D-10', 'D-J', 'D-Q', 'D-K', 'D-A',
              'S-2', 'S-3', 'S-4', 'S-5', 'S-6', 'S-7', 'S-8', 'S-9', 'S-10', 'S-J', 'S-Q', 'S-K', 'S-A',
-             'H-2', 'H-3', 'H-4', 'H-5', 'H-6', 'H-7', 'H-8', 'H-9', 'H-10', 'H-J', 'H-Q', 'H-K', 'H-A' ]
+             'H-2', 'H-3', 'H-4', 'H-5', 'H-6', 'H-7', 'H-8', 'H-9', 'H-10', 'H-J', 'H-Q', 'H-K', 'H-A',]
